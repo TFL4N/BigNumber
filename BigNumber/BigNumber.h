1b118304392d28cb702982860bafa26600efad44
TFL4N <tflan10@gmail.com>
2017-08-19 19:01:50 -0400

    Initial Commit

---
+++ BigNumber/BigNumber.h
@@ -0,0 +1,19 @@
+//
+//  BigNumber.h
+//  BigNumber
+//
+//  Created by Spizzace on 8/19/17.
+//  Copyright © 2017 SpaiceMaine. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BigNumber.
+FOUNDATION_EXPORT double BigNumberVersionNumber;
+
+//! Project version string for BigNumber.
+FOUNDATION_EXPORT const unsigned char BigNumberVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BigNumber/PublicHeader.h>
+
+
