bd28a2a0df829fb536dbd8a032b9d0ccf5788930
TFL4N <tflan10@gmail.com>
2018-03-13 17:58:16 -0400

    Added continuedFractionExpansionOfQuadraticSurd() and findSmallestSolutionOfPellsEquation()

--- BigNumber/BigInt+Utils.swift
+++ BigNumber/BigInt+Utils.swift
@@ -46,7 +46,88 @@ extension BigInt {
 }
 
 // MARK: Static functions
+public typealias ContinedFractionExpansion = (UInt, [UInt])
+
 extension BigInt {
+    
+    // https://proofwiki.org/wiki/Continued_Fraction_Expansion_of_Irrational_Square_Root/Example/13/Convergents
+    public static func continuedFractionExpansionOfQuadraticSurd(_ n: UInt) -> ContinedFractionExpansion {
+        // check if perfect square
+        let root = sqrt(Double(n))
+        let sq_floor_n = floor(root)
+        if root == sq_floor_n {
+            return (UInt(root), [])
+        }
+        
+        ///
+        func findNextTerm(P: UInt, Q: UInt, a: UInt) -> (P: UInt, Q: UInt, a: UInt) {
+            let Pr = a * Q - P
+            let Qr = (n - Pr*Pr) / Q
+            let ar = UInt(floor((sq_floor_n + Double(Pr)) / Double(Qr)))
+            
+            return (Pr, Qr, ar)
+        }
+        
+        ////
+        let a0 = UInt(sq_floor_n)
+        var expansion = [a0]
+        let first = findNextTerm(P: 0, Q: 1, a: a0 )
+        var tuple = first
+        repeat {
+            expansion.append(tuple.a)
+            tuple = findNextTerm(P: tuple.P, Q: tuple.Q, a: tuple.a)
+        } while tuple != first
+        
+        return (expansion.removeFirst(), expansion)
+    }
+    
+    // https://en.wikipedia.org/wiki/Pell%27s_equation#The_smallest_solution_of_Pell_equations
+    // https://proofwiki.org/wiki/Pell%27s_Equation/Examples/13
+    // http://mathworld.wolfram.com/PellEquation.html
+    
+    // x^2 - Dy^2 = 1
+    public static func findSmallestSolutionOfPellsEquation(D: UInt) -> (x: BigInt, y: BigInt) {
+        let (root, expansion) = continuedFractionExpansionOfQuadraticSurd(D)
+        
+        var a0 = BigInt(root)
+        var p0 = BigInt(root)
+        var q0 = BigInt(1)
+        
+        var a1 = BigInt(expansion[0])
+        var p1 = BigInt(a0 * a1 + 1)
+        var q1 = BigInt(a1)
+        
+        let r = expansion.count
+        if (r == 1 || r == 2) && p1*p1 - D*q1*q1 == 1 {
+            return (p1, q1)
+        }
+        
+        var n = 3
+        var stop = false
+        while !stop {
+            let an = BigInt(expansion[(n - 2) % expansion.count])
+            let pn = an * p1 + p0
+            let qn = an * q1 + q0
+            
+            a0 = a1
+            p0 = p1
+            q0 = q1
+            
+            a1 = an
+            p1 = pn
+            q1 = qn
+            
+            if n % r == 0 {
+                stop = p1*p1 - D*q1*q1 == 1
+            }
+            
+            // loop condition
+            n += 1
+        }
+        
+        return (p1, q1)
+    }
+    
     // n! / r!(n-r)!
     public static func combinations(from n: UInt, choose r: UInt) -> BigInt {
         if n == 0 {
